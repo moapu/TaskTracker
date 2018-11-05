6ec40c454e8b8a86ac44c270c433418ff561a210
christophervalko <c625v12@github.com>
2018-11-05 18:38:59 -0500

    Update for if else

--- speechrecognition.py
+++ speechrecognition.py
@@ -1,4 +1,14 @@
+# Project:Speech Recognition Time App
+# Purpose Details: Listen and read in user speech to use as a timer
+# Course: 412
+# Author: Chris Valko, Mostafa apu
+# Date Developed: 10/30/2018
+# Last Date Changed: 11/03/2018
+# Rev: 1
+
 import speech_recognition as sr
+
+
 # get audio from the microphone
 r = sr.Recognizer()  # RECOGNIZE
 
@@ -6,9 +16,14 @@ with sr.Microphone() as source:
     print("Speak:")
     audio = r.listen(source)
 try:
-    print("You said " + r.recognize_google(audio))
+    if r.recognize_google(audio) == "test":
+        print("You said " + r.recognize_google(audio))
+    elif r.recognize_google(audio) != "test":
+        print(r.recognize_google(audio))
+
 except sr.UnknownValueError:
     print("Could not understand audio")
 except sr.RequestError as e:
     print("Could not request results; {0}".format(e))
+
 
