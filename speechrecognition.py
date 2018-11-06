63ce0fb1df0b2c851314321d38bee2a4e3145259
Mostafa Apu <mja5612@psu.edu>
2018-11-06 16:14:38 -0500

    indent properly formatted.

--- speechrecognition.py
+++ speechrecognition.py
@@ -8,29 +8,29 @@
 
 import speech_recognition as sr
 
+
 class SpeechAppetizer:
-# get audio from the microphone
+    # get audio from the microphone
 
-	r = sr.Recognizer()  # RECOGNIZE
+    r = sr.Recognizer()  # RECOGNIZE
 
-	def google_api(self):
-		with sr.Microphone() as source:
-			self.r.adjust_for_ambient_noise(source)
-			print("Speak:")
-			audio = self.r.listen(source)
-		try:
-			if self.r.recognize_google(audio) == "test":
-				print("You said " + self.r.recognize_google(audio))
-			elif self.r.recognize_google(audio) != "test":
-				print(self.r.recognize_google(audio))
+    def google_api(self):
+        with sr.Microphone() as source:
+            self.r.adjust_for_ambient_noise(source)
+            print("Speak:")
+            audio = self.r.listen(source)
+        try:
+            if self.r.recognize_google(audio) == "test":
+                print("You said " + self.r.recognize_google(audio))
+            elif self.r.recognize_google(audio) != "test":
+                print(self.r.recognize_google(audio))
 
-		except sr.UnknownValueError:
-			print("Could not understand audio")
-		except sr.RequestError as e:
-			print("Could not request results; {0}".format(e))
+        except sr.UnknownValueError:
+            print("Could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
 
 
 if __name__ == "__main__":
-
-	s = SpeechAppetizer()
-	s.google_api()
+    s = SpeechAppetizer()
+    s.google_api()
