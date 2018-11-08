99517f65da390795bdd729fa391fcaf120a425fa
Mostafa Apu <mja5612@psu.edu>
2018-11-08 18:07:16 -0500

    modularized

--- speechrecognition.py
+++ speechrecognition.py
@@ -5,63 +5,51 @@
 # Date Developed: 10/30/2018
 # Last Date Changed: 11/03/2018
 # Rev: 1
+# =============================
 
+from enum import Enum
 import speech_recognition as sr
 
 
-class SpeechAppetizer:
-    # get audio from the microphone
+class API(Enum):
+    """ equivalent to Java 'Enum' """
+    GOOGLE = 1
+    BING = 2
+    SPHINX = 3
 
-    r = sr.Recognizer()  # RECOGNIZE
 
-    def google_api(self):
+class SpeechApp:
+
+    def __init__(self):
+        """ RECOGNIZER """
+        self.r = sr.Recognizer()
+
+    def mic_input(self):
+        """ return the microphone input """
         with sr.Microphone() as source:
             self.r.adjust_for_ambient_noise(source)
             print("Speak:")
-            audio = self.r.listen(source)
-        try:
-            if self.r.recognize_google(audio) == "test":
-                print("You said " + self.r.recognize_google(audio))
-            elif self.r.recognize_google(audio) != "test":
-                print(self.r.recognize_google(audio))
+            return self.r.listen(source)
 
+    def recognizer(self, audio, api):
+        """ recognizes with different APIs """
+        if api == API.GOOGLE:
+            return self.r.recognize_google(audio)
+        elif api == API.SPHINX:
+            return self.r.recognize_sphinx(audio)
+
+    def main(self):
+        """ prints out what the user says for now"""
+        try:
+            audio = self.mic_input()
+            recognized_audio = self.recognizer(audio, API.GOOGLE)
+            print(recognized_audio)
         except sr.UnknownValueError:
             print("Could not understand audio")
         except sr.RequestError as e:
             print("Could not request results; {0}".format(e))
 
-    def sphinx_api(self):
-        with sr.Microphone() as source:
-            self.r.adjust_for_ambient_noise(source)
-            print("Speak:")
-            audio = self.r.listen(source)
-
-        try:
-            print("You said " + self.r.recognize_sphinx(audio))
-        except sr.UnknownValueError:
-            print("Could not understand audio")
-        except sr.RequestError as e:
-            print("Could not request results; {0}".format(e))
-
-    def bing_api(self):
-         with sr.Microphone() as source:
-             self.r.adjust_for_ambient_noise(source)
-             print("Speak:")
-             audio = self.r.listen(source)
-         try:
-             if self.r.recognize_bing(audio) == "test":
-                 print("You said " + self.r.recognize_bing(audio))
-             elif self.r.recongize_bing(audio) != "test":
-                 print(self.r.recognize_bing(audio))
-
-         except sr.UnknownValueError:
-             print("Could not understand audio")
-         except sr.RequestError as e:
-             print("Could not request results; {0}".format(e))
 
 if __name__ == "__main__":
-    s = SpeechAppetizer()
-    s.google_api()
-    #s.bing_api()
-    # s.google_api()
-    #s.sphinx_api()
+    s = SpeechApp()
+    s.main()
