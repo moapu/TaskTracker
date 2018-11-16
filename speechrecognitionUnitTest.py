ca56c293b1134df1b023c9ef443ef4171b1e4e55
unknown <maxsd32>
2018-11-16 02:21:05 -0300

    create unit test

---
+++ speechrecognitionUnitTest.py
@@ -0,0 +1,16 @@
+# Project:Speech Recognition Time App
+# Purpose Details: Unit Test for speech recognition
+# Course: 412
+# Author: Jon Ren
+# Date Developed: 11.16/2018
+# Last Date Changed: 11/16/2018
+# Rev: N/A
+
+#test for SpeechRecognition ~STATUS~#
+#---------------------------~in Progress-----------#
+
+import unittest
+
+class UnitTestSpeechRecognition(unittest.TestCase):
+
+print("Nothing to be tested yet, coming soon!")
