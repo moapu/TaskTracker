9d0666b50c03474a5ffb926359d06cf1455610a8
rvd1995 <rvd5224@psu.edu>
2018-11-22 12:31:10 -0500

    Did the unit testing for speech recognizer.

--- speechrecognitionUnitTest.py
+++ speechrecognitionUnitTest.py
@@ -1,16 +1,36 @@
 # Project:Speech Recognition Time App
 # Purpose Details: Unit Test for speech recognition
 # Course: 412
-# Author: Jon Ren
+# Author: Jon Ren, Raveeja Deshpande
 # Date Developed: 11.16/2018
-# Last Date Changed: 11/16/2018
+# Last Date Changed: 11/22/2018
 # Rev: N/A
 
 #test for SpeechRecognition ~STATUS~#
 #---------------------------~in Progress-----------#
 
 import unittest
+import speech_recognition as sr
+import speechrecognition
 
 class UnitTestSpeechRecognition(unittest.TestCase):
 
-    print("Nothing to be tested yet, coming soon!")
+    def test_recognizer(self):
+        #Inputs and excepted outputs
+        input_audio_filename = 'harvard.wav'
+        r = sr.Recognizer()
+        harvard = sr.AudioFile(input_audio_filename)
+        audio = None
+        with harvard as source:
+            audio = r.record(source)
+        
+        api = speechrecognition.API.GOOGLE
+
+        expected_output = 'the stale smell of old beer lingers it takes heat to bring out the odor a cold dip restores health and zest a salt pickle taste fine with ham tacos al Pastore are my favorite a zestful food is the hot cross bun'
+
+        # Executing function
+        app = speechrecognition.SpeechApp()
+        actual_output = app.recognizer(audio,api)
+
+        #Check if input and output is the same
+        self.assertEqual(expected_output, actual_output)
