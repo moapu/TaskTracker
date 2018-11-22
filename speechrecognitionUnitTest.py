4b056c5dce8ead89ea3c8cb26afed2461ab8f0cc
rvd1995 <rvd5224@psu.edu>
2018-11-22 12:56:07 -0500

    Did the transcribe and recognizer unit testing.

--- speechrecognitionUnitTest.py
+++ speechrecognitionUnitTest.py
@@ -12,25 +12,47 @@
 import unittest
 import speech_recognition as sr
 import speechrecognition
+import os.path
 
 class UnitTestSpeechRecognition(unittest.TestCase):
 
-    def test_recognizer(self):
+    def test_transcribe_and_recognizer(self):
+        """
+        This function will take a prerecord input from a file and check
+        it with the expected transcription.
+
+        Since we are reading an audio file to transcribe, it makes sense
+        to test both the functions in the same test case.
+        """
+
         #Inputs and excepted outputs
         input_audio_filename = 'harvard.wav'
-        r = sr.Recognizer()
-        harvard = sr.AudioFile(input_audio_filename)
-        audio = None
-        with harvard as source:
-            audio = r.record(source)
-        
         api = speechrecognition.API.GOOGLE
 
-        expected_output = 'the stale smell of old beer lingers it takes heat to bring out the odor a cold dip restores health and zest a salt pickle taste fine with ham tacos al Pastore are my favorite a zestful food is the hot cross bun'
+        expected_output = 'the stale smell of old beer lingers'
 
         # Executing function
         app = speechrecognition.SpeechApp()
+        audio = app.transcribe_from_file(input_audio_filename)
         actual_output = app.recognizer(audio,api)
 
         #Check if input and output is the same
         self.assertEqual(expected_output, actual_output)
+
+    def test_save_audio_to_file(self):
+        """
+        This funciton will read audio from a file. It will check if
+        the output file exists.
+        """
+
+        #Inputs and expected outputs
+        input_audio_filename = 'harvard.wav'
+        output_audio_filename = 'audio/mic_input_1.wav'
+
+        #Execute function
+        app = speechrecognition.SpeechApp()
+        audio = app.transcribe_from_file(input_audio_filename)
+        app.save_audio_to_file(audio)
+
+        #Check if the file exists
+        self.assertTrue(os.path.isfile(output_audio_filename))
