7ae9c0625f17fbcd2e060874182f1635f7183176
c625v12 <cjvalko@comcast.net>
2018-11-26 19:33:10 -0500

    Fixed spacing

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 import subprocess
 import sys
 
+
 def install(package):
+
     subprocess.call([sys.executable, "-m", "pip", "install", package])
 
+
 if __name__ == '__main__':
+
     install('pyttsx3')
     install('pypiwin32')
     install("pymongo")
