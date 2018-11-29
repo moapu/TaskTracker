56f54b1ec196101ed5725700913ef16e20bf9ab8
rvd1995 <rvd5224@psu.edu>
2018-11-29 13:55:26 -0500

    Add files via upload

---
+++ saveToMongoUnitTest.py
@@ -0,0 +1,89 @@
+# Project:Speech Recognition Time App
+# Purpose Details: Unit Test for saveToMongo
+#
+#
+# Date Developed: 11/28/2018
+# Last Date Changed: 11/28/2018
+# Rev: n/a
+
+#test for saveToMongo
+
+import unittest
+import saveToMongo
+import time
+
+
+class UnitTestsaveToMongo(unittest.TestCase):
+
+    def test_init(self):
+       '''
+       This will initialize MongoDB class and verify 
+       that it is not None
+       '''
+       
+       #Execute
+       mongo_db = saveToMongo.MongoDB()
+
+       #Verify mongo_db is not None
+       self.assertIsNotNone(mongo_db)
+
+    def test_current_time(self):
+       '''
+       This tests if the current time is the same
+       string representation
+       '''
+       #Inputs and expected output
+       expected_output = time.strftime('%Y-%m-%d %I:%M:%S %p')
+       #Execute function
+       actual_output = saveToMongo.MongoDB().current_time()
+       #Check if input and output is same
+       self.assertEqual(actual_output, expected_output)
+
+       
+    def test_save(self):
+       '''
+       This tests if the duration is saved against the timer name.
+       '''
+       timer_name = 'timer_name'
+       duration = 5
+
+       class MockCollection():
+          def __init__(self):
+              self.data = []
+
+          def insert_one(self,value):
+               self.data.append(value)
+
+       #Execute function
+       mongo_db = saveToMongo.MongoDB()
+       mongo_db._MongoDB__collection = MockCollection()
+       mongo_db.save(timer_name, duration)
+
+       #Assert collection is not empty
+       self.assertGreater(len(mongo_db._MongoDB__collection.data),0)
+
+    def test_drop_collection(self):
+       '''
+       This tests if the collection is empty. It defins a 
+       MockCollection that starts.with a filled list. Calling
+       drop() on it will empty the collection. Again, here we are 
+       not testing mongodb. We just need to verify if the drop()
+       method is called on the collection
+       '''
+       #Expected function
+       expected_collection_count = 0
+
+       class MockCollection():
+          def __init__(self):
+              self.data = [1, 2, 3]
+
+          def drop(self):
+           	   self.data.clear()
+
+       #Execute function
+       mongo_db = saveToMongo.MongoDB()
+       mongo_db._MongoDB__collection = MockCollection()
+       mongo_db.drop_collection()
+
+       #Assert collection is not empty
+       self.assertEqual(len(mongo_db._MongoDB__collection.data),expected_collection_count)
